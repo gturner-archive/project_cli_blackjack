3ca62677f7693fd2812b9a17b1ac789330fb6db6
essian <jessicampdennis@hotmail.com>
2016-07-06 00:19:54 +0100

    adds pseudocode for blackjack

---
+++ pseudocode.rb
@@ -0,0 +1,31 @@
+
+1. Initialize deck
+2. initialize dealer
+3. initialize player
+
+  
+4. deal 2 to player
+5. deal 2 to dealer
+   display cards
+6. if dealer has 21 dealer wins, everyone else loses
+7. if player 21 player wins, dealer loses
+
+
+  loop 
+   player makes move (hit or pass)
+     check for blackjack - player wins
+     check for bust - dealer wins
+     play again?
+   end
+
+   until dealer has >= 17
+    add dealer card
+    check for blackjack - dealer wins
+    check  for bust - player wins
+  end
+
+  compare scores and declare winner
+
+   
+  end
+  
